196ab3930a1e30e9dfc6f049ff48025f6cfaf5b8
enoch ampong <enochampong0@gmail.com>
2023-07-05 12:50:54 +0200

    changes

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("kunstaushang JS imported successfully!");
 });
+document.addEventListener("DOMContentLoaded", function() {
+  let slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+  let slideInterval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
+
+  function showSlide(index) {
+    slides[currentSlide].classList.remove("active");
+    slides[index].classList.add("active");
+    currentSlide = index;
+  }
+
+  function nextSlide() {
+    let nextIndex = (currentSlide + 1) % slides.length;
+    showSlide(nextIndex);
+  }
+});
